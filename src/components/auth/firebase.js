0e7d1c563d709420fd9a0198dbdd0065bd0e99a6
SanjuManna <sanjumanna2201@gmail.com>
2023-04-20 16:31:42 +0530

    Commit

--- src/components/auth/firebase.js
+++ src/components/auth/firebase.js
@@ -17,13 +17,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBewoXYyA6-F70ixHK_II5JT0Xq2jU7dyA",
-    authDomain: "quora-clone-68ab1.firebaseapp.com",
-    projectId: "quora-clone-68ab1",
-    storageBucket: "quora-clone-68ab1.appspot.com",
-    messagingSenderId: "438447826262",
-    appId: "1:438447826262:web:cbb5352eb05bdcb8e0a2f7",
-    measurementId: "G-9S9P8FVEGJ"
+    apiKey: "AIzaSyBvmqKhK6pLqS1swiVu7wialNdbP7E3F34",
+    authDomain: "quora-2-fd8ad.firebaseapp.com",
+    projectId: "quora-2-fd8ad",
+    storageBucket: "quora-2-fd8ad.appspot.com",
+    messagingSenderId: "905615334898",
+    appId: "1:905615334898:web:020b2f140f921377291f6b",
+    measurementId: "G-87N1EQ3PNV"
 };
 
 
@@ -47,3 +47,5 @@ export {
     signOut,
     onAuthStateChanged
 };
+
+
